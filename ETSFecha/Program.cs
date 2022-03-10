f8ceedda58d894b544e4c4dd13817b71bdbae641
fa_bi <fa_bi@DESKTOP-KDO9DS4>
2022-03-10 13:03:07 +0000

    Agregar archivos de proyecto.

---
+++ ETSFecha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETSFecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
