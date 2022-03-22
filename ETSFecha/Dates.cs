2ae4ac36e8bae905d858a9cb5dd67617eaf51f9f
Fabio Castro Jiménez <fabiocastrojimenez2003@hotmail.com>
2022-03-22 13:11:12 +0000

    22/03/2022

---
+++ ETSFecha/Dates.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETSFecha
+{
+    class Dates
+    {
+        public static void ComprobarInformacion(Program.Fecha[] fechasComprobar)
+        {
+            for (int contador = 0; contador < fechasComprobar.Length; contador++)
+            {
+                if (fechasComprobar[contador].AntesODespues == 0)
+                {
+                    DateTime fecha = new DateTime(fechasComprobar[contador].Anno, fechasComprobar[contador].Mes, fechasComprobar[contador].Dia);
+                    DateTime fechaHoy = DateTime.Today;
+
+                    int dias = ((fechaHoy.Day) - fechasComprobar[contador].Dia);
+                    int anno = fechasComprobar[contador].Anno + fechaHoy.Year;
+
+                    Console.WriteLine("Hay " + anno + " años y " + dias + " días de diferencia en la fecha número {0}",contador + 1);
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private static int CalcularAnnosAC(DateTime fecha)
+        {
+            DateTime fechaHoy = DateTime.Today;
+            int anno = (fecha.Year + fechaHoy.Year);
+
+            return anno; 
+        }
+
+
+
+
+
+
+
+
+
+        //bool correct = DateTime.IsLeapYear(400);
+
+
+    }
+}
--- ETSFecha/Program.cs
+++ ETSFecha/Program.cs
@@ -4,9 +4,25 @@ namespace ETSFecha
 {
     class Program
     {
+        public struct Fecha
+        {
+            public int Anno;
+            public int Mes;
+            public int Dia;
+            public int AntesODespues; 
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fecha[] fechas = new Fecha[2];
+
+            fechas[0].Anno = 1900;
+            fechas[0].Mes = 2;
+            fechas[0].Dia = 10;
+            fechas[0].AntesODespues = 0;
+
+
+            Dates.ComprobarInformacion(fechas);
         }
     }
 }
